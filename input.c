c8f03da0826127a4a806662fd38f35be8fa97f25
kieranoski <kfowlds1812@gmail.com>
2025-03-03 17:07:59 +0000

    Add delta back for keyboard

--- input.c
+++ input.c
@@ -161,8 +161,8 @@ int input_update(struct wiimote_state *state,
       bool moving = event.analog_motion_event.moving;
       switch (event.analog_motion_event.motion) {
       case INPUT_ANALOG_MOTION_POINTER:
-        /* pointer_delta_x = event.analog_motion_event.delta_x; */
-        /* pointer_delta_y = event.analog_motion_event.delta_y; */
+        pointer_delta_x = event.analog_motion_event.delta_x;
+        pointer_delta_y = event.analog_motion_event.delta_y;
         /* printf("pointer: %f %f\n", event.analog_motion_event.x, */
         /*        event.analog_motion_event.y); */
         /* pointer_x = event.analog_motion_event.x; */
@@ -278,15 +278,13 @@ int input_update(struct wiimote_state *state,
     }
   }
 
-  /* pointer_delta_x += ir_right * 0.004 - ir_left * 0.004; */
-  /* pointer_delta_y += ir_up * 0.004 - ir_down * 0.004; */
+  pointer_delta_x += ir_right * 0.004 - ir_left * 0.004;
+  pointer_delta_y += ir_up * 0.004 - ir_down * 0.004;
 
-  /* pointer_x = fmax(-pointer_margin, */
-  /*                  fmin(1.0 + pointer_margin, pointer_x + pointer_delta_x));
-   */
-  /* pointer_y = fmax(-pointer_margin, */
-  /*                  fmin(1.0 + pointer_margin, pointer_y + pointer_delta_y));
-   */
+  pointer_x = fmax(-pointer_margin,
+                   fmin(1.0 + pointer_margin, pointer_x + pointer_delta_x));
+  pointer_y = fmax(-pointer_margin,
+                   fmin(1.0 + pointer_margin, pointer_y + pointer_delta_y));
 
   set_motion_state(state, pointer_x, pointer_y);
   /* set_exact_pointer_state(state, pointer_x, pointer_y); */
